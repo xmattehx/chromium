717e50885a5d23a4a810a2c0611cd8c2945c60fb
xmattehx <matty323@gmail.com>
2013-10-30 12:06:35 +1100

    Update codec.sh

--- codec.sh
+++ codec.sh
@@ -156,18 +156,18 @@ rm -rf chrome-unstable
 
 
 ## Google Talk
-#echo "Downloading Google Talk plugin"
-#curl -z "/opt/talk-bin.deb" -o "/opt/talk-bin.deb" -L $TALK
+echo "Downloading Google Talk plugin"
+curl -z "/opt/talk-bin.deb" -o "/opt/talk-bin.deb" -L $TALK
 
-#python /opt/deb2tar.py /opt/talk-bin.deb /opt/talk.tar.gz
-#rm /opt/talk-bin.deb
-#rm -rf /opt/google/talkplugin
+python /opt/deb2tar.py /opt/talk-bin.deb /opt/talk.tar.gz
+rm /opt/talk-bin.deb
+rm -rf /opt/google/talkplugin
 
-#tar -xvf /opt/talk.tar.gz -C /
-#rm /opt/google/chrome/pepper/libnpgoogletalk.so
-#ln -s /opt/google/talkplugin/libnpgoogletalk.so /opt/google/chrome/pepper/libnpgoogletalk.so
-#rm /opt/google/chrome/pepper/libnpgtpo3dautoplugin.so
-#ln -s /opt/google/talkplugin/libnpgtpo3dautoplugin.so /opt/google/chrome/pepper/libnpgtpo3dautoplugin.so
+tar -xvf /opt/talk.tar.gz -C /
+rm /opt/google/chrome/pepper/libnpgoogletalk.so
+ln -s /opt/google/talkplugin/libnpgoogletalk.so /opt/google/chrome/pepper/libnpgoogletalk.so
+rm /opt/google/chrome/pepper/libnpgtpo3dautoplugin.so
+ln -s /opt/google/talkplugin/libnpgtpo3dautoplugin.so /opt/google/chrome/pepper/libnpgtpo3dautoplugin.so
 
 #rm /opt/talk.tar.gz
 
